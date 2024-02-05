b29601c54a45fe3aff0f25d9a6ba98eeec542add
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2024-02-05 12:49:08 +0900

    Update k8s_sample.py

--- dags/k8s_sample.py
+++ dags/k8s_sample.py
@@ -6,10 +6,8 @@ from kubernetes.client import models as k8s
 #    KubernetesPodOperator,
 #)
 
-#from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
-#    KubernetesPodOperator,
-#)
 
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 
 resources = k8s.V1ResourceRequirements(
     limits={"memory": "1Gi", "cpu": "1"},
